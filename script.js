708943002a6bc732b63f2634d28629235142b146
raghib-shahriar <shahraghib.ece16@gmail.com>
2023-07-09 00:01:54 +0600

    complete but faulty

--- script.js
+++ script.js
@@ -21,5 +21,30 @@ document.onkeydown = function (e) {
         nyan.style.left = (nyanX - 120) + "px";
     }
 }
+// game over calc
+setTimeout(() => {
+    nyan=document.querySelector(".nyan");
+    blackCat=document.querySelector(".blackCat");
+    gameOver=document.querySelector(".gameOver");
+    nX =parseInt(window.getComputedStyle(nyan,null).getPropertyValue("left")); 
+    bX =parseInt(window.getComputedStyle(blackCat,null).getPropertyValue("left"));
+    dX=Math.abs(nX-bX);
 
+    if(dX<73){
+        gameOver.innerHTML="GAMEOVER";
+        blackCat.classList.remove("animateBlackCat");
+    }
+    else if (dX < 145 && cross) {
+        score=score+1;
+        updateScore(score);
+        cross=false;
+        setTimeout(()=>{
+            cross=true;
+        },1000);
+    }
+}, 10);
+
+function updateScore(score){
+    document.querySelector("#scoreCont").innerHTML="Your Score:" +score;
+}
 
